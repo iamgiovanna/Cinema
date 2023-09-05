babca71a27ef6ca2180aa991c971db4a874a5950
Giovanna Oliveira <112138845+iamgiovanna@users.noreply.github.com>
2023-09-05 08:42:15 -0300

    banco no sql

---
+++ cinema.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Set-2023 às 13:29
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `cinema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cod_cliente` varchar(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `data_nasc` date NOT NULL,
+  `rg` varchar(20) NOT NULL,
+  `cpf` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `filme`
+--
+
+CREATE TABLE `filme` (
+  `cod_filme` varchar(5) NOT NULL,
+  `sinopse` varchar(200) NOT NULL,
+  `duracao` time NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `classificacao_ind` varchar(2) NOT NULL,
+  `Diretor` varchar(50) NOT NULL,
+  `Genero` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sala`
+--
+
+CREATE TABLE `sala` (
+  `N_sala` varchar(5) NOT NULL,
+  `cod_Sala` varchar(5) NOT NULL,
+  `Desc_sala` varchar(150) NOT NULL,
+  `Localizacao` varchar(150) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cod_cliente`);
+
+--
+-- Índices para tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD PRIMARY KEY (`cod_filme`);
+
+--
+-- Índices para tabela `sala`
+--
+ALTER TABLE `sala`
+  ADD PRIMARY KEY (`cod_Sala`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
